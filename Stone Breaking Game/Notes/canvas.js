1d46a0e4651dc2757584ffac751a306929b75314
minhh <minhhaivuhd@gmail.com>
2019-04-30 15:33:47 -0400

    create demo animation with JS

---
+++ Stone Breaking Game/Notes/canvas.js
@@ -0,0 +1,50 @@
+/*
+This script is to  create a basic animation which the box moving from left to right.
+The purpose of activity is to practice JS on topics:
+    - Canvas
+    - Animation
+April 2019, Developed by: Minh Hai Vu
+ */
+
+//Constants of Animation
+const DURATION = 3000; //3 seconds
+const DISTANCE = 300;
+const NUMBER_OF_INTERVAL = 200;
+
+var canvas = document.getElementById("canvas").getContext("2d");
+var state = 0;
+
+//Animation
+function moving() {
+    state = (state + DISTANCE/NUMBER_OF_INTERVAL) % DISTANCE; //New position
+
+    //Clear canvas
+    canvas.clearRect(0,0,500,500);
+
+    //Save the current state
+    canvas.save();
+
+    //Draw rectangle
+    canvas.translate(90,200);
+    canvas.fillStyle = "#F31997";
+    canvas.fillRect(state,0,15,20);
+
+    //Restore the canvas state
+    canvas.restore();
+	
+
+}
+
+//MAIN
+var status = true;
+var animation = setInterval(moving, DURATION/NUMBER_OF_INTERVAL);
+window.animation;
+window.addEventListener("keydown",function(){
+	if (status){
+		window.clearInterval(animation); //Trigger Animation
+	}else{
+		animation = setInterval(moving, DURATION/NUMBER_OF_INTERVAL);
+		window.animation; //Turn off Animation
+	};
+	status = !status;
+});
