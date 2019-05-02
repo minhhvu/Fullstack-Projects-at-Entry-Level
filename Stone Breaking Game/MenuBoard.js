e6db8f4767d5351f90cbd8ec978bbe4c695a594d
minhh <minhhaivuhd@gmail.com>
2019-05-02 13:50:11 -0400

    remove files

--- Stone Breaking Game/MenuBoard.js
+++
@@ -1,9 +0,0 @@
-class MenuBoard {
-    constructor(){
-        this.score = 0;
-        this.time = 90;
-        this.level = 1;
-        this.isPlaying = true;
-        this.isMusic = true;
-    }
-}
