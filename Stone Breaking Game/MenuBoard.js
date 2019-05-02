6b517a41b33e22672558243978076899e31fdfb3
minhh <minhhaivuhd@gmail.com>
2019-05-02 13:55:26 -0400

    create MenuBoard class

---
+++ Stone Breaking Game/MenuBoard.js
@@ -0,0 +1,9 @@
+class MenuBoard {
+    constructor(){
+        this.score = 0;
+        this.isPlaying = true;
+        this.timer = TIME_PER_TURN;
+        this.music = true;
+        this.level = 1;
+    }
+}
